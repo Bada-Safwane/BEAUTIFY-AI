798e104ece758b84ee51d7f6cc7ab29fdf9020db
Safwane Bada <safwaneb1@gmail.com>
2025-12-14 23:32:21 +0100

    A

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -2,7 +2,7 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { v4 as uuidv4 } from 'uuid';
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION || 'us-east-1',
+    region: process.env.AWS_REGION || 'eu-north-1',
     credentials: {
         accessKeyId: process.env.ID,
         secretAccessKey: process.env.SECRET,
